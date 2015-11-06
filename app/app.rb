f56d5e303dbd08471a4d913848118645ab035ea4
Roi Driscoll <roi.driscoll@gmail.com>
2015-11-06 13:38:06 +0000

    added set:view folder

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,12 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require './app/data_mapper_setup.rb'
+
 require_relative 'models/link'
 
+
 class BookmarkManager < Sinatra::Base
-  set :views, proc {File.join(root, '..', 'views')}
+    set :views, proc {File.join(root, '..', 'views')}
   get '/' do
     erb(:index)
   end
@@ -19,26 +21,22 @@ class BookmarkManager < Sinatra::Base
     erb(:'links/new_link')
   end
 
-  get '/links/new' do
-    erb(:'links/new_link')
-  end
-
   post '/links/new/add' do
-    Link.select {|link| @old_link = link if link.title == params[:title]}
-
-    Tag.select {|tag| @old_tag = tag if tag.name == params[:name]}
-
-    if @old_link != nil && @old_tag != nil
-      (@old_link.tags << @old_tag).save
-    elsif @old_link != nil && @old_tag == nil
-      (@old_link.tags << Tag.create(name: params[:name])).save
-    elsif @old_link == nil && @old_tag != nil
-      new_link = Link.create(title: params[:title], url: params[:url])
-      (new_link.tags << @old_tag).save
+    @tag = Tag.create(name: params[:name])
+    Link.find do |link|
+      if link.title == params[:title]
+        @existing_link = link
+      end
+    end
+    if @existing_link != nil
+      @existing_link.tags << @tag
+      @existing_link.save
     else
         new_link = Link.create(title: params[:title], url: params[:url])
-        (new_link.tags << Tag.create(name: params[:name])).save
+        new_link.tags << @tag
+        new_link.save
     end
+
     redirect :links
   end
 
